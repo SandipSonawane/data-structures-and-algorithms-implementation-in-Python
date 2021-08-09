ce2adbb4da0a1578cfe3de7282652f57923744c8
Sandip Sonawane <sandips.nemesis@gmail.com>
2021-08-08 19:39:58 -0500

    initial commit

--- Array/350. Intersection of Two Arrays II.py
+++ Array/350. Intersection of Two Arrays II.py
@@ -1,1 +1,16 @@
 # 350. Intersection of Two Arrays II
+
+class Solution:
+    def intersect(self, nums1, nums2):
+        intersect = []
+        for num in nums2:
+            if num in nums1:
+                intersect.append(num)
+        return intersect
+
+
+nums1 = [1,2,2,1]
+nums2 = [2,2]
+
+sol = Solution()
+sol.intersect(nums1, nums2)
